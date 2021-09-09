ae40b75ef7ead676e3fd7616b8deb77af98e941e
olga <yaolmi@yandex.ru>
2021-09-09 16:58:45 +0700

    change directory for test on MAMP

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ const webpack = require("webpack-stream");
 const browsersync = require("browser-sync");
 
 //const dist = "./dist/";
-const dist = "C:/MAMP/htdocs/test";
+const dist = "C:/MAMP/htdocs/test/01";
 
 gulp.task("copy-html", () => {
     return gulp.src("./src/index.html")
